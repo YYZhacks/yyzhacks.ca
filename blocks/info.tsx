06f533c667718f0af63c254eaeee80ec091bcea9
arav <arav.1tb@gmail.com>
2022-09-03 17:15:30 -0400

    fix

--- blocks/info.tsx
+++ blocks/info.tsx
@@ -5,11 +5,11 @@ function Faq() {
         <div className="ml-12 mt-24 text-white leading-loose">
         <h1 className="font-semibold text-2xl"> What's a Hackathon? </h1>
         <p className="text-lg"> 
-        Hackathons don't include any <span className="bg-yellow-200 text-black font-bold"> "Hacking" </span> <br />   in the way you think.
+        Hackathons don't include any <span className="bg-yellow-200 text-black font-bold"> "Hacking" </span>   in the way you think. <br  />
         Insted Hackathons are where <span className="bg-yellow-200 text-black font-bold"> People of all Skill Levels </span> <br />
         come together to build something with <span className="bg-yellow-200 text-black font-bold"> with code in a short span of time </span> <br />
   
-        <a href="https://hackthenorth.com" className=" underline text-m font-bold text-yellow-200"> Take a look at Hack the North, another in-person Hackathon in Waterloo!</a>
+        <a href="https://hackthenorth.com" data-splitbee-event="Click CTA" className=" underline text-m font-bold text-yellow-200"> Take a look at Hack the North, another in-person Hackathon in Waterloo!</a>
         <hr className="border-4 border-blue-500" /> <br />
         </p> 
     
--- blocks/sponsers.tsx
+++ blocks/sponsers.tsx
@@ -1,16 +1,13 @@
-function Nav() {
+function Sponsers() {
   return (
-    <nav className="flex justify-center items-center mt-6 gap-x-4 text-white">
+    <div className="flex justify-center items-center mt-6 gap-x-4 text-white">
       <div className="mb-12 space-x-6 sm:space-y-4 md:space-y-4"> 
-      <!--Make a Image tag that links to a icon--!>
-      <img src="ht"
         <span> Schedule</span>
         <span> FAQ</span>
         <span> Sponsers</span>
         <button className="border-4 border-yellow-200 hover:text-black hover:font-semibold hover:bg-orange-100 mt-6 p-2.5 w-24 sm:w-24 flex-wrap"> Sign up </button>
         </div>
-    </nav>
+    </div>
     )
 }
-
-export default Nav
+export default Sponsers
