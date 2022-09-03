67d096efe3cfede27770fe55d719ee1b5e7af423
arav <arav.1tb@gmail.com>
2022-09-03 14:14:49 -0400

    fix nvbar

--- blocks/navbar.tsx
+++ blocks/navbar.tsx
@@ -2,9 +2,12 @@ function Nav() {
   return (
   
     
-    <nav className="flex justify-center items-center bg-blue-300">
+    <nav className="flex justify-center items-center mt-6 gap-x-4 text-white">
       <div className="mb-12 space-x-6 sm:space-y-4 md:space-y-4"> 
         <span> Schedule</span>
+        <span> FAQ</span>
+        <span> Sponsers</span>
+        <button className="border-4 border-yellow-200 hover:text-black hover:font-semibold hover:bg-orange-100 mt-6 p-2.5 w-24 sm:w-24 flex-wrap"> Sign up </button>
         </div>
     </nav>
     )
